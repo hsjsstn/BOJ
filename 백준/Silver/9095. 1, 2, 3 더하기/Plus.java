b1a2b377d505c2e253f0c036cc1a71aef9849d37
S <105033044+hsjsstn@users.noreply.github.com>
2024-06-21 23:12:04 +0900

    9095 Solved

---
+++ 백준/Silver/9095. 1, 2, 3 더하기/Plus.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Plus {
+    static int count = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> picked = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int num = sc.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            numbers.add(sc.nextInt());
+        }
+
+
+        for (int i : numbers) {
+            for (int j = i; j > 0; j--) {
+                pick(3, picked, i, j);
+            }
+            System.out.println(count);
+            count = 0;
+        }
+
+        sc.close();
+    }
+
+     public static void pick(int n, ArrayList<Integer> picked, int m, int toPick) {
+        int lastIndex, smallest;
+
+        if (toPick == 0) {
+            int sum = 0;
+
+            for (int i : picked) {
+                sum += i + 1;
+                // System.out.print(i+1 + " ");
+                if (sum > m) {
+                    return;
+                }
+            }
+            // System.out.println();
+            // System.out.println("m: "+ m +" | sum: " + sum);
+            if (sum == m) {
+                count++;
+                // System.out.println("!!!");
+            }
+                
+            return;
+        }
+
+        lastIndex = m - toPick- 1;
+
+        smallest = 0;
+
+        for (int i = smallest; i < n; i++) {
+            picked.add(i);
+            pick(n, picked, m, toPick - 1);
+            picked.remove(picked.size() - 1);
+        }
+    }
+}
